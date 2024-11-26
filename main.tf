5de704d4005056d51603d457c669b6a9e9cf2e89
vaibhav <vaibhavpanwar270@gmail.com>
2024-11-26 22:57:12 +0530

    newrepo

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "azurerm_resource_group" "devops" {
+  name     = "devopsinsider"
+  location = "eastus"
+}
+resource "azurerm_resource_group" "devops2" {
+  name     = "devopsinsider3"
+  location = "centralindia"
+}
